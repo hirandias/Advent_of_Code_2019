c036619b04c9338d54c1c4d3dab1caf8bf9d24fb
Hiran <57793499+hirandias@users.noreply.github.com>
2020-06-24 23:31:19 +0530

    Create day04.py

---
+++ Day_04/day04.py
@@ -0,0 +1,61 @@
+input1 = 272091
+input2 = 815432
+
+
+def same_adjacent_digits(num):
+    num_list = [int(x) for x in str(num)]
+    for i in range(5):
+        if num_list[i] == num_list[i+1]:
+            return True
+    return False
+
+
+def never_decrease(num):
+    num_list = [int(x) for x in str(num)]
+    for i in range(5):
+        if num_list[i] > num_list[i + 1]:
+            return False
+    return True
+
+
+def larger_group(num):
+    num_list = [int(x) for x in str(num)]
+    for i in range(5):
+        if num_list[i] == num_list[i+1]:
+            if i == 0 and num_list[0] != num_list[2]:
+                return False
+            elif i == 4 and num_list[4] != num_list[3]:
+                return False
+            elif num_list[i-1] != num_list[i] and num_list[i+1] != num_list[i+2]:
+                return False
+    return True
+
+
+def password_count1(in1, in2):
+    count = 0
+    for psw in range(in1, in2+1):
+        if len(str(psw)) != 6:
+            continue
+        elif not same_adjacent_digits(psw):
+            continue
+        elif not never_decrease(psw):
+            continue
+        count += 1
+    return count
+
+
+def password_count2(in1, in2):
+    count = 0
+    for psw in range(in1, in2+1):
+        if len(str(psw)) != 6:
+            continue
+        elif not never_decrease(psw):
+            continue
+        elif larger_group(psw):
+            continue
+        count += 1
+    return count
+
+
+print(password_count1(input1, input2))
+print(password_count2(input1, input2))
