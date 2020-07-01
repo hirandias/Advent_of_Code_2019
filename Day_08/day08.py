f22059cad1858a0c1d20db6c88b41172d109a2a9
Hiran <57793499+hirandias@users.noreply.github.com>
2020-07-01 13:48:56 +0530

    Create day08.py

---
+++ Day_08/day08.py
@@ -0,0 +1,51 @@
+#Day 08
+with open('data.txt', 'r') as f:
+    data = f.readline()
+
+
+def image_by_layers(image):
+    layers = []
+    for i in range(0, len(image), 150):
+        layers.append(list(image[i:i + 150]))
+    return layers
+
+
+def corrupted_check(layers):
+    count = float('inf')
+    layer_index = None
+
+    for index, layer in enumerate(layers):
+        if layer.count('0') < count:
+            count = layer.count('0')
+            layer_index = index
+
+    return layers[layer_index].count('1') * layers[layer_index].count('2')
+
+
+image_layers = image_by_layers(data)
+print(corrupted_check(image_layers))
+
+
+def decode_image(layers):
+    decoded = []
+    for i, pixel in enumerate(layers[0]):
+        if pixel == '0':
+            decoded.append(' ')
+        elif pixel == '1':
+            decoded.append('#')
+        elif pixel == '2':
+            for j in range(1, len(layers)):
+                if layers[j][i] == '0':
+                    decoded.append(' ')
+                    break
+                elif layers[j][i] == '1':
+                    decoded.append('#')
+                    break
+
+    for k in range(0, len(decoded), 25):
+        for m in range(25):
+            print(decoded[k+m], end='')
+        print('')
+
+
+decode_image(image_layers)
